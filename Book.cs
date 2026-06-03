7cbfbea13d0840ae8bceab898c8caef276916ba1
celsve-cmd <celsve@gmail.com>
2026-06-03 11:07:32 +0200

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -19,17 +19,3 @@ namespace Oppgave_1__Småoppgaver
         }
     }
 }
-
-    internal class Book
-    {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public int YearOfPublicity { get; set; }
-
-        public Book(string title, string author, int yearOfPublicity)
-        {
-            Title = title;
-            Author = author;
-            YearOfPublicity = yearOfPublicity;
-        }
-    }
